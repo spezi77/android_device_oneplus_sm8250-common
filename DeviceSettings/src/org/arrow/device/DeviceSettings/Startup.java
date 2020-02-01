53361bee350aca17d453af697204ee99568d06e0
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-02-01 19:35:22 +0530

    sm8250-common: DeviceSettings: Add Touchscreen Gesture controls
    Imported from LineagePart, adapted to LineageHW implementation present in fwb instead of lineage-sdk
    
    Change-Id: I08064ca6c7a7e58783847677843522b818c583de
    TouchscreenGestureSettings class written by:
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Signed-off-by: Dr-chen99 <dakaizhe@gmail.com>

--- DeviceSettings/src/org/arrow/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/org/arrow/device/DeviceSettings/Startup.java
@@ -34,6 +34,7 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
 
         boolean enabled = false;
+		TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_HBM_SWITCH, false);
         if (enabled) {
@@ -62,6 +63,12 @@ public class Startup extends BroadcastReceiver {
         DeviceSettings.restoreVibStrengthSetting(context);
     }
 
+
+import org.arrow.device.DeviceSettings.TouchscreenGestureSettings;
+
+    private static final String TAG = "BootReceiver";
+    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
+
     private void restore(String file, boolean enabled) {
         if (file == null) {
             return;
@@ -76,5 +83,15 @@ public class Startup extends BroadcastReceiver {
             return;
         }
         Utils.writeValue(file, value);
+    }
+	
+	private boolean hasRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
+    }
+
+    private void setRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
     }
 }
