chandu-A12/device_oneplus_sm8250-common

sm8250-common: DeviceSettings: Import CustomSeekbarPreference & Custom SwitchPreference * from https://github.com/Havoc-OS/android_vendor_support/commits/eleven/src/com/havoc/support/preferences.
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Alex <du.alexcruz@gmail.com>
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

DeviceSettings/src/org/havoc/device/DeviceSettings/DeviceSettings.java
@@ -47,8 +47,8 @@ import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.TwoStatePreference;
-import com.havoc.support.preferences.CustomSeekBarPreference;
-import com.havoc.support.preferences.SwitchPreference;
+import org.havoc.device.DeviceSettings.CustomSeekBarPreference;
+import org.havoc.device.DeviceSettings.SwitchPreference;
 
 import org.havoc.device.DeviceSettings.FileUtils;
 import org.havoc.device.DeviceSettings.Doze.DozeSettingsActivity;

DeviceSettings/src/org/havoc/device/DeviceSettings/Doze/DozeSettingsFragment.java
@@ -39,7 +39,7 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
-import com.havoc.support.preferences.SwitchPreference;
+import org.havoc.device.DeviceSettings.SwitchPreference;
 
 import org.havoc.device.DeviceSettings.R;
 

DeviceSettings/src/org/havoc/device/DeviceSettings/Preference/AutoHBMThresholdPreference.java
@@ -25,7 +25,7 @@ import android.util.Log;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceViewHolder;
-import com.havoc.support.preferences.CustomSeekBarPreference;
+import org.havoc.device.DeviceSettings.CustomSeekBarPreference;
 
 public class AutoHBMThresholdPreference extends CustomSeekBarPreference {
 

DeviceSettings/src/org/havoc/device/DeviceSettings/Preference/PreferenceCategory.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.havoc.device.DeviceSettings;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import org.havoc.device.DeviceSettings.R;
+
+public class PreferenceCategory extends androidx.preference.PreferenceCategory {
+
+    public PreferenceCategory(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+
+    public PreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+}

DeviceSettings/src/org/havoc/device/DeviceSettings/Preference/SwitchPreference.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.havoc.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.R;
+
+import org.havoc.device.DeviceSettings.VibrationUtils;
+
+public class SwitchPreference extends androidx.preference.SwitchPreference {
+
+    private final Context mContext;
+
+    public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        mContext = context;
+    }
+
+    public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SwitchPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.switchPreferenceStyle,
+                android.R.attr.switchPreferenceStyle));
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+
+    public SwitchPreference(Context context) {
+        this(context, null);
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+
+    @Override
+    protected void performClick(View view) {
+        super.performClick(view);
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
+    }
+}

DeviceSettings/src/org/havoc/device/DeviceSettings/Preference/VibratorStrengthPreference.java
@@ -22,9 +22,9 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Toast;
-import com.havoc.support.preferences.CustomSeekBarPreference;
+import org.havoc.device.DeviceSettings.CustomSeekBarPreference;
 
-import com.havoc.support.R;
+import org.havoc.device.DeviceSettings.R;
 
 public class VibratorStrengthPreference extends CustomSeekBarPreference {
 

DeviceSettings/src/org/havoc/device/DeviceSettings/VibrationUtils.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.havoc.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+public class VibrationUtils {
+
+    public static void doHapticFeedback(Context context, int effect) {
+        final Vibrator mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        final boolean hapticEnabled = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+        if (mVibrator != null && mVibrator.hasVibrator() && hapticEnabled) {
+            mVibrator.vibrate(VibrationEffect.get(effect));
+        }
+    }
+}

spezi77/android_device_oneplus_sm8250-common

DeviceSettings/src/org/arrow/device/DeviceSettings/DeviceSettings.java
@@ -47,8 +47,8 @@ import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.TwoStatePreference;
-import com.arrow.support.preferences.CustomSeekBarPreference;
-import com.arrow.support.preferences.SwitchPreference;
+import org.arrow.device.DeviceSettings.CustomSeekBarPreference;
+import org.arrow.device.DeviceSettings.SwitchPreference;
 
 import org.arrow.device.DeviceSettings.FileUtils;
 import org.arrow.device.DeviceSettings.Doze.DozeSettingsActivity;

DeviceSettings/src/org/arrow/device/DeviceSettings/Doze/DozeSettingsFragment.java
@@ -39,7 +39,7 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
-import com.arrow.support.preferences.SwitchPreference;
+import org.arrow.device.DeviceSettings.SwitchPreference;
 
 import org.arrow.device.DeviceSettings.R;
 

DeviceSettings/src/org/arrow/device/DeviceSettings/Preference/AutoHBMThresholdPreference.java
@@ -25,7 +25,7 @@ import android.util.Log;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceViewHolder;
-import com.arrow.support.preferences.CustomSeekBarPreference;
+import org.arrow.device.DeviceSettings.CustomSeekBarPreference;
 
 public class AutoHBMThresholdPreference extends CustomSeekBarPreference {
 

DeviceSettings/src/org/arrow/device/DeviceSettings/Preference/PreferenceCategory.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arrow.device.DeviceSettings;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import org.arrow.device.DeviceSettings.R;
+
+public class PreferenceCategory extends androidx.preference.PreferenceCategory {
+
+    public PreferenceCategory(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+
+    public PreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+}

DeviceSettings/src/org/arrow/device/DeviceSettings/Preference/SwitchPreference.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2020 Arrow-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arrow.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.R;
+
+import org.arrow.device.DeviceSettings.VibrationUtils;
+
+public class SwitchPreference extends androidx.preference.SwitchPreference {
+
+    private final Context mContext;
+
+    public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        mContext = context;
+    }
+
+    public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SwitchPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, TypedArrayUtils.getAttr(context,
+                androidx.preference.R.attr.switchPreferenceStyle,
+                android.R.attr.switchPreferenceStyle));
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+
+    public SwitchPreference(Context context) {
+        this(context, null);
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+
+    @Override
+    protected void performClick(View view) {
+        super.performClick(view);
+        VibrationUtils.doHapticFeedback(mContext, VibrationEffect.EFFECT_CLICK);
+    }
+}

DeviceSettings/src/org/arrow/device/DeviceSettings/Preference/VibratorStrengthPreference.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (C) 2021 The Arrow-OS Project
  *
@@ -22,9 +23,9 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Toast;
-import com.arrow.support.preferences.CustomSeekBarPreference;
+import org.arrow.device.DeviceSettings.CustomSeekBarPreference;
 
-import com.arrow.support.R;
+import org.arrow.device.DeviceSettings.R;
 
 public class VibratorStrengthPreference extends CustomSeekBarPreference {
 

DeviceSettings/src/org/arrow/device/DeviceSettings/VibrationUtils.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020 Arrow-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arrow.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+public class VibrationUtils {
+
+    public static void doHapticFeedback(Context context, int effect) {
+        final Vibrator mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        final boolean hapticEnabled = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+        if (mVibrator != null && mVibrator.hasVibrator() && hapticEnabled) {
+            mVibrator.vibrate(VibrationEffect.get(effect));
+        }
+    }
+}