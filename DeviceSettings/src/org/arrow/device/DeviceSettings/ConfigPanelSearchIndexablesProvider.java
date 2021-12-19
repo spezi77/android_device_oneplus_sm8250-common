evilvenom/device_oneplus_sm8250-common

sm8250-common: Introduce DeviceSettings Includes: * Automatic DC-Dimming preference * HBM & Auto HBM + QS tile * Dolby Atmos + QS tile * Ambient display + QS tile (OnePlusDoze merged into DeviceSettings) * FPS Info + QS tile * Touchscreen gestures preference * GameMode toggle + QS tile * Vibrator strength preference * OnePlus logo for DeviceSettings icon * DeviceSettings QS tile * Log QS tile
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
Co-authored-by: ritujb <ritujbeniwal@gmail.com>
Co-authored-by: SKULSHADY <anushekprasal@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: AnierinB <anierin@evolution-x.org>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

DeviceSettings/src/org/havoc/device/DeviceSettings/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.havoc.device.DeviceSettings;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.main,
+                    DeviceSettingsActivity.class.getName(),
+                    R.drawable.ic_settings_device),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "org.havoc.device.DeviceSettings";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}

DeviceSettings/src/org/havoc/device/DeviceSettings/Doze/DozeTileService.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.havoc.device.DeviceSettings.Doze;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+
+@TargetApi(24)
+public class DozeTileService extends TileService {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        Intent OnePlusDoze = new Intent(this, DozeSettingsActivity.class);
+        OnePlusDoze.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(OnePlusDoze);
+    }
+}

DeviceSettings/src/org/havoc/device/DeviceSettings/Preference/AutoHBMThresholdPreference.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.havoc.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import com.havoc.support.preferences.CustomSeekBarPreference;
+
+public class AutoHBMThresholdPreference extends CustomSeekBarPreference {
+
+    private static int mMinVal = 0;
+    private static int mMaxVal = 60000;
+    private static int mDefVal = 20000;
+
+    public AutoHBMThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = 1000;
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        mMinValue = mMinVal;
+        mMaxValue = mMaxVal;
+        mDefaultValueExists = true;
+        mDefaultValue = mDefVal;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(DeviceSettings.KEY_AUTO_HBM_THRESHOLD, "20000"));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(DeviceSettings.KEY_AUTO_HBM_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}

DeviceSettings/src/org/havoc/device/DeviceSettings/Services/HBMModeTileService.java
@@ -1,0 +1,88 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.havoc.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import java.lang.IllegalArgumentException;
+
+@TargetApi(24)
+public class HBMModeTileService extends TileService {
+
+    private Intent mHbmIntent;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        tryStopService();
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateState();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        // NOTE: reverse logic, enabled reflects the state before press
+        Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "5");
+        if (!enabled) {
+            mHbmIntent = new Intent(this,
+                    org.havoc.device.DeviceSettings.HBMModeService.class);
+            this.startService(mHbmIntent);
+        }
+        updateState();
+    }
+
+    private void updateState() {
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        if (!enabled) tryStopService();
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    private void tryStopService() {
+        if (mHbmIntent == null) return;
+        this.stopService(mHbmIntent);
+        mHbmIntent = null;
+    }
+}

DeviceSettings/src/org/havoc/device/DeviceSettings/SuTask.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.havoc.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.havoc.device.DeviceSettings.R;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, R.string.cannot_get_su,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}

spezi77/android_device_oneplus_sm8250-common

DeviceSettings/src/org/arrow/device/DeviceSettings/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arrow.device.DeviceSettings;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.main,
+                    DeviceSettingsActivity.class.getName(),
+                    R.drawable.ic_settings_device),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "org.arrow.device.DeviceSettings";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}

DeviceSettings/src/org/arrow/device/DeviceSettings/Doze/DozeTileService.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.arrow.device.DeviceSettings.Doze;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+
+@TargetApi(24)
+public class DozeTileService extends TileService {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        Intent OnePlusDoze = new Intent(this, DozeSettingsActivity.class);
+        OnePlusDoze.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(OnePlusDoze);
+    }
+}

DeviceSettings/src/org/arrow/device/DeviceSettings/Preference/AutoHBMThresholdPreference.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.arrow.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import com.arrow.support.preferences.CustomSeekBarPreference;
+
+public class AutoHBMThresholdPreference extends CustomSeekBarPreference {
+
+    private static int mMinVal = 0;
+    private static int mMaxVal = 60000;
+    private static int mDefVal = 20000;
+
+    public AutoHBMThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = 1000;
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        mMinValue = mMinVal;
+        mMaxValue = mMaxVal;
+        mDefaultValueExists = true;
+        mDefaultValue = mDefVal;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(DeviceSettings.KEY_AUTO_HBM_THRESHOLD, "20000"));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(DeviceSettings.KEY_AUTO_HBM_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}

DeviceSettings/src/org/arrow/device/DeviceSettings/Services/HBMModeTileService.java
@@ -1,0 +1,88 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.arrow.device.DeviceSettings;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import java.lang.IllegalArgumentException;
+
+@TargetApi(24)
+public class HBMModeTileService extends TileService {
+
+    private Intent mHbmIntent;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        tryStopService();
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateState();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        // NOTE: reverse logic, enabled reflects the state before press
+        Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "5");
+        if (!enabled) {
+            mHbmIntent = new Intent(this,
+                    org.arrow.device.DeviceSettings.HBMModeService.class);
+            this.startService(mHbmIntent);
+        }
+        updateState();
+    }
+
+    private void updateState() {
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        if (!enabled) tryStopService();
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    private void tryStopService() {
+        if (mHbmIntent == null) return;
+        this.stopService(mHbmIntent);
+        mHbmIntent = null;
+    }
+}

DeviceSettings/src/org/arrow/device/DeviceSettings/SuTask.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arrow.device.DeviceSettings;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.arrow.device.DeviceSettings.R;
+
+public abstract class SuTask<Params> extends AsyncTask<Params, Void, Boolean> {
+    private Context mContext;
+
+    public SuTask(Context context) {
+        super();
+        mContext = context;
+    }
+
+    abstract protected void sudoInBackground(Params... params)
+            throws SuShell.SuDeniedException;
+
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        try {
+            sudoInBackground(params);
+            return true;
+        } catch (SuShell.SuDeniedException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+            Toast.makeText(mContext, R.string.cannot_get_su,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}